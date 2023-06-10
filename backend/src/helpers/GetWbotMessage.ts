90e17aff2a084687ceded80303c8260ab31ad2e0
plwdesign <128205620+plwdesign@users.noreply.github.com>
2023-06-10 09:53:16 -0300

    Update GetWbotMessage.ts

--- backend/src/helpers/GetWbotMessage.ts
+++ backend/src/helpers/GetWbotMessage.ts
@@ -1,5 +1,5 @@
-import { proto } from "@adiwajshing/baileys";
-import WALegacySocket from "@adiwajshing/baileys"
+import { proto } from "@whiskeysockets/baileys";
+import WALegacySocket from "@whiskeysockets/baileys"
 import Ticket from "../models/Ticket";
 import GetTicketWbot from "./GetTicketWbot";
 import AppError from "../errors/AppError";
